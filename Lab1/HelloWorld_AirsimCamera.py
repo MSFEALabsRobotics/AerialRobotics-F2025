2d0e91af78a67e201eedf3e4c6501e94f5d51ce5
MSFEALabsRobotics <sb137@aub.edu.lb>
2025-09-02 23:24:03 +0300

    Update HelloWorld_AirsimCamera.py

--- Lab1/HelloWorld_AirsimCamera.py
+++ Lab1/HelloWorld_AirsimCamera.py
@@ -2,18 +2,17 @@ import airsim
 import cv2
 import numpy as np
 
-client = airsim.MultirotorClient(ip="WINDOWSIP", port=5000)
+# Replace with Windows host IP
+client = airsim.MultirotorClient(ip="WINDOWSIP")
 client.confirmConnection()
 
-while True:
-    responses = client.simGetImages([
-        airsim.ImageRequest("0", airsim.ImageType.Scene, False, False)
-    ])
-    img1d = np.frombuffer(responses[0].image_data_uint8, dtype=np.uint8)
-    img_rgb = img1d.reshape(responses[0].height, responses[0].width, 3)
-    cv2.imshow("AirSim Frame", img_rgb)
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+# Get image from front-center camera
+responses = client.simGetImages([
+    airsim.ImageRequest("0", airsim.ImageType.Scene, False, False)
+])
 
-cv2.destroyAllWindows()
+img1d = np.frombuffer(responses[0].image_data_uint8, dtype=np.uint8)
+img_rgba = img1d.reshape(responses[0].height, responses[0].width, 3)
+
+cv2.imshow("AirSim Camera", img_rgba)
+cv2.waitKey(0)
