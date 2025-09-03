ec36605f6ebb1c3eed9124403b382309ba865252
MSFEALabsRobotics <sb137@aub.edu.lb>
2025-09-03 07:24:45 +0300

    Update HelloWorld_SITL.py

--- Lab1/HelloWorld_SITL.py
+++ Lab1/HelloWorld_SITL.py
@@ -18,20 +18,3 @@ master.mav.command_long_send(
     mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
     0, 1, 0, 0, 0, 0, 0, 0)
 print("Arming...")
-
-
-
-while True:
-    # Get the next message (blocking)
-    msg = master.recv_match(blocking=True)
-    if not msg:
-        continue
-
-    msg_type = msg.get_type()
-    if msg_type == "ATTITUDE":
-        print(f"Roll: {msg.roll:.2f}, Pitch: {msg.pitch:.2f}, Yaw: {msg.yaw:.2f}")
-    elif msg_type == "GLOBAL_POSITION_INT":
-        lat = msg.lat / 1e7
-        lon = msg.lon / 1e7
-        alt = msg.relative_alt / 1000.0  # in meters
-        print(f"Lat: {lat:.6f}, Lon: {lon:.6f}, Alt: {alt:.2f} m")
